a87e2e7d746519d782c19c7cd764989ffc2dc590
arfashaikh123 <shaikharfa2005@gmail.com>
2026-02-16 13:25:39 +0530

    Update app_new.py

--- new_update/app_new.py
+++ new_update/app_new.py
@@ -172,7 +172,7 @@ Respond ONLY with topic name.
 """
 
                     response = client.chat.completions.create(
-                        model="llama-3.3-8b-instant",
+                        model="llama-3.1-8b-instant",
                         messages=[{"role": "user", "content": prompt}],
                         temperature=0,
                         max_tokens=50
